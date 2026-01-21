ca9a711807b3f2089291bfdc7476870cd7b875c8
azureramakrishna <83686453+azureramakrishna@users.noreply.github.com>
2026-01-21 11:00:08 +0530

    Update provider.tf

--- Terraform/WindowsVM/provider.tf
+++ Terraform/WindowsVM/provider.tf
@@ -13,7 +13,7 @@ provider "azurerm" {
   features {}
 
   subscription_id = "2e28c82c-17d7-4303-b27a-4141b3d4088f"
-  client_id       = "95dbbdc9-d160-460b-b8cc-86cdb55d351c"
-  client_secret   = "jSK8Q~yg_9Ct1NHYAuSuOrlj5e258oq93BCTEaN2"
-  tenant_id       = "459865f1-a8aa-450a-baec-8b47a9e5c904"
+  client_id       = ""
+  client_secret   = ""
+  tenant_id       = ""
 }
