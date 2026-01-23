c2879e379e2b04a7d713369dcd199cb5e69ec04c
azureramakrishna <83686453+azureramakrishna@users.noreply.github.com>
2026-01-23 09:55:56 +0530

    Update main.tf

--- Terraform/AppService/main.tf
+++ Terraform/AppService/main.tf
@@ -1,31 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "4.47.0"
-    }
-  }
-}
-
-# Configure the Microsoft Azure Provider
-provider "azurerm" {
-  resource_provider_registrations = "none" # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
-  features {}
-
-  subscription_id = ""
-}
-
-# terraform backend configuration
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "saanvikit-rg"
-    storage_account_name = "saanvikittf"       # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
-    container_name       = "tfstate"           # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-    key                  = "terraform.tfstate" # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
-  }
-}
-
-
 # Create a resource group 01
 resource "azurerm_resource_group" "rg-1" {
   name     = var.ressource_group_name_01
@@ -57,4 +29,5 @@ resource "azurerm_app_service" "app_service" {
    site_config {
     always_on = true
   }
+
 }
