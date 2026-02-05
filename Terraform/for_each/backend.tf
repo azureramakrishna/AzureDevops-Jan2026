0375b4e134594c97bfaee7885a0f2b2cb702a62a
Ramakrishna A <aruvaramakrishna@gmail.com>
2026-02-05 09:52:14 +0530

    Github actions workflow

--- Terraform/for_each/backend.tf
+++ Terraform/for_each/backend.tf
@@ -2,8 +2,8 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "saanvikit-rg"
-    storage_account_name = "saanvikittf"               # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
-    container_name       = "tfstate"                   # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    storage_account_name = "saanvikittf"              # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "tfstate"                  # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
     key                  = "github.terraform.tfstate" # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
   }
 }
